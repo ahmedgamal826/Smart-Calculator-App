cbe72a772e9e3db4867dc923afe6c1fc1b340e33
Ahmed Elnemr <aelnemr711@gmail.com>
2024-11-30 21:32:26 +0200

    Finish Smart Calculator App

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,8 @@
-// import 'package:calculator_app_task1/features/splash/presentation/views/splash_view.dart';
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const CalculatorApp());
-// }
-
-// class CalculatorApp extends StatelessWidget {
-//   const CalculatorApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: SplashView(),
-//     );
-//   }
-// }
-
 import 'package:calculator_app_task1/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart'; // import the package
 
 void main() {
-  runApp(
-    DevicePreview(
-      // Wrap your app with DevicePreview
-      builder: (context) => const CalculatorApp(), // Pass the app widget
-      enabled: true, // Enable preview only in debug mode
-    ),
-  );
+  runApp(const CalculatorApp());
 }
 
 class CalculatorApp extends StatelessWidget {
@@ -39,7 +13,35 @@ class CalculatorApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashView(),
-      builder: DevicePreview.appBuilder, // Add this builder
     );
   }
 }
+
+/////////////// Using Device Preview ////////////////
+
+// import 'package:calculator_app_task1/features/splash/presentation/views/splash_view.dart';
+// import 'package:flutter/material.dart';
+// import 'package:device_preview/device_preview.dart'; // import the package
+
+// void main() {
+//   runApp(
+//     DevicePreview(
+//       // Wrap your app with DevicePreview
+//       builder: (context) => const CalculatorApp(), // Pass the app widget
+//       enabled: true, // Enable preview only in debug mode
+//     ),
+//   );
+// }
+
+// class CalculatorApp extends StatelessWidget {
+//   const CalculatorApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: SplashView(),
+//       builder: DevicePreview.appBuilder, // Add this builder
+//     );
+//   }
+// }
